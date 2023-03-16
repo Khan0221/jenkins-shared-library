f3235d676102a0475060999d83449531e5a04580
vikash <vikash@gmail.com>
2023-03-16 20:15:27 +0530

    jenkins shared libs

--- vars/StaticCodeAnalysis.groovy
+++ vars/StaticCodeAnalysis.groovy
@@ -1,11 +1,7 @@
 def call(credentialsId) {
-    stage('Static code analysis') {
-        steps {
-            script {
-                withSonarQubeEnv(credentialsId: credentialsId) {
-                    sh 'mvn clean package sonar:sonar'
-                }
-            }
-        }
-    }
+
+withSonarQubeEnv(credentialsId: credentialsId) {
+    sh 'mvn clean package sonar:sonar'
+}
+
 }
