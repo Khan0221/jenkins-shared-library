55f4ddc8570176d982722db2817826f733440e01
vikash <vikash@gmail.com>
2023-03-17 12:08:37 +0530

    jenkins shared libs

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -14,7 +14,7 @@
 
 def call(String hubUser, String region, String aws_account_id) {
 
-    withCredentials([amazonWebService(credentialsId: 'my-aws-creds', region: ${region})]) 
+    withCredentials([amazonWebService(credentialsId: 'my-aws-creds', region: '${region}' )]) 
     {
     sh "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com"
     }
